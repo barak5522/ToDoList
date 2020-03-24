4100e6238b3cc91c43c421adc164b219d139366c
barak <barakproj5.gmail.com>
2020-03-24 12:58:28 +0200

    new desing #10

--- todo-list/src/components/Task.js
+++ todo-list/src/components/Task.js
@@ -2,8 +2,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { deleteTask, editTask } from '../actions/taskActions.js'
 import './Task.css'
-
-
+import { Card, CardContent, Checkbox } from '@material-ui/core';
 
 class Task extends Component {
     render() {
@@ -11,26 +10,33 @@ class Task extends Component {
         const tasks = this.props.tasks.map(task => {
             if (!task.hide) {
                 return (
-                    <div key={task.id}>
-                        <input type="checkbox" checked={task.confirm} onChange={() => {
-                            this.props.editTask(task.id, task.task, !task.confirm);
-                            }}></input>
-                        <label>{task.task}</label>
-                        <img className="action-icon" src="https://i.ya-webdesign.com/images/how-to-edit-a-png-file-4.png"
-                        alt="" data-task={task.task} onClick={() => {
-                            let editedTask = prompt("edit the task", task.task)
-                            if (editedTask !== null || editedTask !== task.task) {
-                                this.props.editTask(task.id, editedTask, task.confirm)
-                            }
-                        }}></img>
-                        <img className="action-icon" src="https://cdn2.iconfinder.com/data/icons/cleaning-19/30/30x30-10-512.png"
-                        alt="" onClick={() => {this.props.deleteTask(task.id)}}></img>
-                    </div>
+                    <Card key={task.id} className="task">    
+                        <CardContent> 
+                            <Checkbox
+                                color="primary"
+                                checked={task.confirm}
+                                onChange={() => {
+                                    this.props.editTask(task.id, task.task, !task.confirm);
+                                }}
+                            />   
+                            <label>{task.task}</label>
+                            <img className="action-icon" src="https://cdn2.iconfinder.com/data/icons/cleaning-19/30/30x30-10-512.png"
+                            alt="" onClick={() => {this.props.deleteTask(task.id)}}></img>
+                            <img className="action-icon" src="https://i.ya-webdesign.com/images/how-to-edit-a-png-file-4.png"
+                            alt="" data-task={task.task} onClick={() => {
+                                let editedTask = prompt("edit the task", task.task)
+                                if (editedTask !== null || editedTask !== task.task) {
+                                    this.props.editTask(task.id, editedTask, task.confirm)
+                                }
+                            }}></img>
+                        </CardContent>
+                    </Card>
                 )
             }
+            return null
         });
         return (
-            <div className="tasks collection">
+            <div className="tasks-collection">
                 {tasks}
             </div>
         )
